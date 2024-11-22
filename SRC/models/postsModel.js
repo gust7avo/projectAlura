05a061118dd39ef646bc435c74d21cebbdf0c11b
Gustavo <ggtavares0@gmail.com>
2024-11-22 17:49:05 -0300

    Primeiro commit

---
+++ SRC/models/postsModel.js
@@ -0,0 +1,23 @@
+import 'dotenv/config';
+import { ObjectId } from "mongodb"
+import conectarAoBanco from "../config/dbcConfig.js"
+const conexao = await conectarAoBanco(process.env.STRING_CONEXAO)
+
+export async function getTodosPosts() {
+    const db = conexao.db("projeto-Alura")
+    const colecao = db.collection("posts")
+    return colecao.find().toArray()
+}
+
+export async function criarPosts(novoPost) {
+    const db = conexao.db("projeto-Alura")
+    const colecao = db.collection("posts")
+    return colecao.insertOne(novoPost)
+}
+
+export async function atualizarPost(id, novoPost) {
+    const db = conexao.db("projeto-Alura");
+    const colecao = db.collection("posts")
+    const objID = ObjectId.createFromHexString(id)
+    return colecao.updateOne({_id: new ObjectId(objID)}, {$set:novoPost})
+}
